a43fb180ec83cbda92d37f03fe5d4fd843a1534e
Yash_Varpe <yashvarpe2005@gmail.com>
2025-02-21 22:38:30 +0530

    Add TypeScript type definitions for Cards component

--- components/ui/cards.tsx
+++ components/ui/cards.tsx
@@ -1,20 +1,34 @@
 import React, { forwardRef } from "react";
 import Image from "next/image";
 
-const Cards = forwardRef(({ id, frontSrc, frontAlt, backText }, ref) => {
-  return (
-    <div className="card" ref={ref} id={id}>
-      <div className="flip-card-inner">
-        <div className="flip-card-front">
-          <Image src={frontSrc} alt={frontAlt} fill style={{ objectFit: "cover" }} />
-        </div>
-        <div className="flip-card-back">
-          <p>{backText}</p>
-        </div>
-      </div>
-    </div>
-  );
-});
+interface CardsProps {
+	id: string;
+	frontSrc: string;
+	frontAlt: string;
+	backText: string;
+}
+
+const Cards = forwardRef<HTMLDivElement, CardsProps>(
+	({ id, frontSrc, frontAlt, backText }, ref) => {
+		return (
+			<div className="card" ref={ref} id={id}>
+				<div className="flip-card-inner">
+					<div className="flip-card-front">
+						<Image
+							src={frontSrc}
+							alt={frontAlt}
+							fill
+							style={{ objectFit: "cover" }}
+						/>
+					</div>
+					<div className="flip-card-back">
+						<p>{backText}</p>
+					</div>
+				</div>
+			</div>
+		);
+	}
+);
 
 Cards.displayName = "Cards";
 export default Cards;
